f798699b579ccb7d3f35af7865114b2bdb2647fd
Floyd Haremsa <floyd.h@gmx.de>
2022-10-25 18:28:01 +0200

    Rebootstrap project with azure extension

--- src/my-first-function/index.ts
+++
@@ -1,9 +0,0 @@
-import { AzureFunction } from '@azure/functions'
-
-export const index: AzureFunction = (context) => {
-    context.log('My first function was triggered, id: ', context.invocationId)
-
-    const name = context.req?.query.name
-    if (name) return Promise.resolve({ body: `¡Hola, ${name}!, Que tal?` })
-    else return Promise.resolve({ body: `¿Como te llamas?` })
-}
