0911b95cd88761fad642a776b42ac851177aa18c
Floyd Haremsa <floyd.h@gmx.de>
2022-10-26 12:01:10 +0200

    Clean up function

--- MyFirstFunction/index.ts
+++ MyFirstFunction/index.ts
@@ -1,26 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
-const httpTrigger: AzureFunction = async function (
-    context: Context,
-    _req: HttpRequest
-) {
+const handler: AzureFunction = async (context, req: HttpRequest) => {
     context.log('My first function was triggered, id: ', context.invocationId)
 
-    const name = context.req?.query.name || context.req?.body.name
+    const name = req.query.name || req.body.name
     context.res = {
+        // status: 200, /* Defaults to 200 */
         body: name ? `¡Hola, ${name}!, Que tal?` : `¿Como te llamas?`,
     }
-
-    // context.log('HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    // context.res = {
-    //     // status: 200, /* Defaults to 200 */
-    //     body: responseMessage
-    // };
 }
 
-export default httpTrigger
+export default handler
